680df3c2588e052cc3daf6e578908d8c2bee9c3c
Hp <Hp@DESKTOP-VQO4T31>
2024-03-20 13:40:21 +0100

    my first react commit app

---
+++ src/header.js
@@ -0,0 +1,10 @@
+
+
+function Header(){
+    return(
+      <h1 className="header">
+        header
+      </h1>
+    );
+  }
+  export default Header;
---
+++ src/mainContent.js
@@ -0,0 +1,10 @@
+
+
+function MainContent(){
+    return(
+      <h1 id="content">
+        Main Content
+      </h1>
+    );
+  }
+  export default MainContent;
